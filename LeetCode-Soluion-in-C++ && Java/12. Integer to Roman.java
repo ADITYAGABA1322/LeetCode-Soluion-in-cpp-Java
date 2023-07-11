e4aee798799e7bfa13dc9b863114f41c51997bd2
Aditya <97380595+ADITYAGABA1322@users.noreply.github.com>
2023-07-11 15:20:23 +0530

    Update 12. Integer to Roman.java

--- LeetCode-Soluion-in-C++ && Java/12. Integer to Roman.java
+++ LeetCode-Soluion-in-C++ && Java/12. Integer to Roman.java
@@ -15,4 +15,30 @@ C++:
 
 
 
+
+// Time Complexity : O(n) where n is the length of the string and space complexity is O(1)
+
+
+
+
+
+class Solution {
+public:
+    string intToRoman(int num) {
+        string result = "";  // variable to store the result
+        vector<int> values = {1000, 900, 500, 400, 100, 90, 50, 40, 10 , 9, 5, 4, 1};  // vector to store the values
+        vector<string> symbols = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X" , "IX", "V", "IV", "I"};  // vector to store the symbols
+        for(int i = 0; i < values.size(); i++){  // iterate through the vectors
+            while(num >= values[i]){  // iterate until the number is greater than or equal to the current value
+                num -= values[i];  // update the number
+                result += symbols[i];  // update the result
+            }
+        }
+        return result;  // return the result
+    }
+};
+
+
+
+
 Java:
