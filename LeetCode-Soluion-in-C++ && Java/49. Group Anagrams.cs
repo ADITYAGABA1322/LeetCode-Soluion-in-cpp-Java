f68ec20d26a4e879274d212d7fcf5fd38df8e876
Aditya <97380595+ADITYAGABA1322@users.noreply.github.com>
2023-07-28 00:10:38 +0530

    Update 49. Group Anagrams.cs

--- LeetCode-Soluion-in-C++ && Java/49. Group Anagrams.cs
+++ LeetCode-Soluion-in-C++ && Java/49. Group Anagrams.cs
@@ -39,6 +39,47 @@ public:
 
 Java:
 
+// Time Complexity : O(n * klogk) where n is the length of the vector strs and k is the length of the string in the vector strs and space complexity is O(n)
+
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        List<List<String>> res = new ArrayList<>();  // list to store the result
+        Map<String, List<String>> map = new HashMap<>();  // map to store the temporary result
+        for(String s : strs){  // iterate through the vector strs
+            char[] temp = s.toCharArray();  // variable to store the temporary result
+            Arrays.sort(temp);  // sort the string
+            String sorted = new String(temp);  // variable to store the temporary result
+            if(!map.containsKey(sorted)){  // if the map does not contain the key
+                map.put(sorted, new ArrayList<>());  // push the temporary result into the map
+            }
+            map.get(sorted).add(s);  // push the temporary result into the map
+        }
+        for(Map.Entry<String, List<String>> m : map.entrySet()){  // iterate through the map
+            res.add(m.getValue());  // push the temporary result into the result
+        }
+        return res;  // return the result
+    }
+}
+
+2nd Method
+
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        List<List<Integer>> res = new ArrayList<>();
+        Map<String , List<String>> mp = new HashMap<>();
+        for(String s : strs){
+            char[] temp = s.toCharArray();
+            Arrays.sort(temp);
+            String sorted = new String(temp);
+            if(!mp.containsKey(sorted)) mp.put(sorted , new ArrayList<>());
+            mp.get(sorted).add(s);
+        }
+        return new ArrayList<>(mp.values());
+    }
+}
+
+
+
 
 Python:
 
